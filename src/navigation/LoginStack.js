e32dd5d865a7da7cd869e78e600e6552e35ab0aa
Keshav Ashiya <keshavashiya@outlook.com>
2021-02-26 08:16:29 +0530

    Navigation isssue resolved

--- src/navigation/LoginStack.js
+++ src/navigation/LoginStack.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import Routes from './routes';
-import Launch from '../screens/launch/Launch';
+// import Launch from '../screens/launch/Launch';
 import Signup from '../screens/signup/Signup';
 
 const Stack = createStackNavigator();
@@ -9,7 +9,7 @@ const Stack = createStackNavigator();
 export default props => {
 	return (
 		<Stack.Navigator headerMode="none" initialRouteName={Routes.APP_INTRO}>
-			<Stack.Screen name={Routes.APP_INTRO} component={Launch} />
+			{/* <Stack.Screen name={Routes.APP_INTRO} component={Launch} /> */}
 			<Stack.Screen name={Routes.SIGNUP_SCREEN} component={Signup} />
 		</Stack.Navigator>
 	);

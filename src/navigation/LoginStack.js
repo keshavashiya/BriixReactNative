eb0069803b422b3c221c19c25e51ec3349187196
rupesh <rupesh@transformhub.com>
2021-03-04 17:14:11 +0530

    done the splash screen

--- src/navigation/LoginStack.js
+++ src/navigation/LoginStack.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import Routes from './routes';
-// import Launch from '../screens/launch/Launch';
+import Launch from '../screens/launch/Launch';
 import Signin from '../screens/signin/Signin';
 import Signup from '../screens/signup/Signup';
 import ResetPassword from '../screens/ResetPassword/ResetPassword';
@@ -11,7 +11,7 @@ const Stack = createStackNavigator();
 export default props => {
 	return (
 		<Stack.Navigator headerMode="none" initialRouteName={Routes.APP_INTRO}>
-			{/* <Stack.Screen name={Routes.APP_INTRO} component={Launch} /> */}
+			{/*<Stack.Screen name={Routes.APP_INTRO} component={Launch} />*/}
 			<Stack.Screen name={Routes.SIGNIN_SCREEN} component={Signin} />
 			<Stack.Screen name={Routes.SIGNUP_SCREEN} component={Signup} />
 			<Stack.Screen name={Routes.RESET_PASSWORD_SCREEN} component={ResetPassword} />
